be9bcd82bfcba662e4ede2f9df83e3eac3f9e38f
MohamedML <mohamed.moussati.lamhamdi@gmail.com>
2021-09-05 19:30:52 +0100

    Agregado la capacidad de crear nuevas tareas

--- inter.js
+++ inter.js
@@ -10,3 +10,20 @@ function finish(val) {
         doc.style.textDecoration = "line-through";
     }
 }
+function addTask() {
+    var listTask = document.createElement("li");
+    var task = document.createElement("input");
+    var removeButton = document.createElement("button");
+    var finishButton = document.createElement("button");
+    listTask.appendChild(task);
+    listTask.appendChild(removeButton);
+    listTask.appendChild(finishButton);
+    removeButton.textContent = "Delete";
+    finishButton.textContent = "Finish";
+    task.setAttribute("class", "input");
+    removeButton.setAttribute("class", "button");
+    /* removeButton.setAttribute("onclick", "remove()"); */
+    finishButton.setAttribute("class", "button");
+    /* finishButton.setAttribute("onclick", "finish()"); */
+    document.getElementById("ulTag").appendChild(listTask);
+}
